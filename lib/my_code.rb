8ec634021dc3ec292721aa1c828851efcec634a4
Lauren Dove <elledoveinc@gmail.com>
2019-08-21 00:57:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,7 @@ end
 def map_to_change(source_array)
   new = []
   i = 0 
-  while i > source_array.length do
+  while i < source_array.length do
     new.push (source_array [i])
     i +=1 
   end
