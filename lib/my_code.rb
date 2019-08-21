ac25947d5ec2c5920bbb075bdba15539a4a77734
Lauren Dove <elledoveinc@gmail.com>
2019-08-21 01:04:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,5 +32,9 @@ def map_to_square(source_array)
   new = []
   i = 0 
   while i < source_array.length do
-    new.push (source_array [])
+    new.push (source_array [i] **2)
+    i += 1 
+  end 
+  return new 
+end
 # My Code here....
